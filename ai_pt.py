e565f0df3d7e05989c123c4120ab078c76182552
Martin Shin <martinshin95@gmail.com>
2025-03-30 17:47:46 +0800

    update streamlit

--- ai_pt.py
+++ ai_pt.py
@@ -1,20 +1,42 @@
 import cv2
 import streamlit as st
 import numpy as np
+import pandas as pd
 import mediapipe as mp
+import pickle
 
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 
 cap = cv2.VideoCapture(0)
 
-st.title("Webcam Live Feed")
+st.title("Posenet Personal Trainer")
+
+option = st.selectbox(
+    "Choose your personal trainer AI model:",
+    ("Random Forest", "Gradient Boosting"),
+    index=None,
+    placeholder="Select contact method...",
+)
+
+st.write("You selected:", option)
+
+model_dict = {
+    "Random Forest": "rf",
+    "Gradient Boosting": "gb",
+}
+if option is not None:
+    with open(f"{model_dict[option]}.pkl", "rb") as f:
+        model = pickle.load(f)
+
 
 frame_placeholder = st.empty()
 stop_button_pressed = st.button("Stop")
+pt_message_placeholder = st.empty()
+pt_prediction_placeholder = st.empty()
 
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    while cap.isOpened() and not stop_button_pressed:
+    while option and cap.isOpened() and not stop_button_pressed:
         ret, image = cap.read()
 
         if not ret:
@@ -56,9 +78,30 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         )
 
         frame_placeholder.image(image, channels="BGR")
+        try:
+            # Extract Pose landmarks
+            pose = results.pose_landmarks.landmark
+            pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+
+            # Concate rows
+            row = pose_row
+
+            # Make Detections
+            X = pd.DataFrame([row])
+            pose_class = model.predict(X)[0]
+            pose_prob = model.predict_proba(X)[0]
+            if "bad" in pose_class:
+                pt_message_placeholder.write("Upright torso, do not lean your weight on one side")
+            if "good" in pose_class:
+                pt_message_placeholder.write("Good posture, keep it up!")
+            pt_prediction_placeholder.write(f"Pose: {pose_class}, Probability: {round(pose_prob[np.argmax(pose_prob)], 2)}")
+
+        except:
+            pass
 
         if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
             break
+
 
 cap.release()
 # # cv2.destroyAllWindows
